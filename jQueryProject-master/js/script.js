5cab7ac854f2adecf0fa80ba86daca966a4429db
Makii2 <yeisong219@gmail.com>
2017-04-28 14:11:08 -0600

    Add files via upload

---
+++ jQueryProject-master/js/script.js
@@ -0,0 +1,17 @@
+&(document).ready(function(){
+   &(".iniciar").click(function(){
+    
+       var user = "patos";
+       var password = "1234abc";
+       
+       var useringresado = &(".nombre"),val();
+       var passwordingresado = &(."pass").val();
+       
+       if (user ==useringresado && passwordingresa){
+        alert("Bienvenido Pato");
+       }else{
+           alert("Intentelo Nuevamente");
+       }
+       
+   })
+})
